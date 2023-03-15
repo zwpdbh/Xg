e97d67c18e73dafd29a2859ad9c4168bc3da832c
zwpdbh <hyperion_z@outlook.com>
2023-03-15 21:35:52 +0800

    < added editorconfig file

--- src/Xg.Client/Startup.fs
+++ src/Xg.Client/Startup.fs
@@ -1,4 +1,4 @@
-namespace Xg.Client
+﻿namespace Xg.Client
 
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
 open Bolero.Remoting.Client
@@ -9,6 +9,9 @@ module Program =
     let Main args =
         let builder = WebAssemblyHostBuilder.CreateDefault(args)
         builder.RootComponents.Add<Main.MyApp>("#main")
-        builder.Services.AddRemoting(builder.HostEnvironment) |> ignore
+
+        builder.Services.AddRemoting(builder.HostEnvironment)
+        |> ignore
+
         builder.Build().RunAsync() |> ignore
         0
